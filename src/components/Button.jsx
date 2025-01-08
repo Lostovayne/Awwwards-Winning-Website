f55ae95cc80e4104e7456152f073fa023a579d68
Lostovayne <moises.barrera.dev@gmail.com>
2025-01-08 03:45:26 -0300

    Update project dependencies and enhance UI components
    - Added 'react-icons' dependency for icon support.
    - Updated README.md to reflect project name change to "Awwwards Website" and included a new image.
    - Refactored Tailwind CSS configuration for improved readability.
    - Introduced a new Button component for consistent UI.
    - Enhanced Hero component with the new Button and improved text styling.

---
+++ src/components/Button.jsx
@@ -0,0 +1,16 @@
+export const Button = (props) => {
+	const { title, id, rightIcon, leftIcon, containerClass } = props;
+
+	return (
+		<button
+			id={id}
+			className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black ${containerClass}`}>
+			{leftIcon}
+
+			<span className="relative inline-flex overflow-hidden font-general text-xs xl:text-sm uppercase font-semibold">
+				<div>{title}</div>
+			</span>
+			{rightIcon}
+		</button>
+	);
+};
