1c360b804797aba8a891756694b363ab68f62482
Lostovayne <moises.barrera.dev@gmail.com>
2025-01-09 18:46:49 -0300

    Add About component and integrate GSAP animations in App.jsx and Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,23 +1,34 @@
-import { useRef, useState } from "react";
-import gsap from "gsap";
 import { Button } from "@/components/Button";
 import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useEffect, useRef, useState } from "react";
 import { TiLocationArrow } from "react-icons/ti";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Hero = () => {
 	const [currentIndex, setCurrentIndex] = useState(1);
 	const [hasClicked, setHasClicked] = useState(false);
+
 	const [isLoading, setIsLoading] = useState(true);
 	const [loadedVideos, setLoadedVideos] = useState(0);
+
 	const nextVideoRef = useRef(null);
 	const totalVideos = 4;
 
 	const handleVideoLoaded = () => setLoadedVideos((prev) => prev + 1);
+
+	useEffect(() => {
+		if (loadedVideos === totalVideos - 1) {
+			setIsLoading(false);
+		}
+	}, [loadedVideos]);
+
 	const handleMiniVdClick = () => {
 		setHasClicked(true);
 		setCurrentIndex((prevIndex) => (prevIndex % totalVideos) + 1);
 	};
-	const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
 
 	/**
 	 * Utiliza GSAP para animar los videos cuando se hace clic en el video miniatura.
@@ -30,20 +41,20 @@ const Hero = () => {
 	useGSAP(
 		() => {
 			if (hasClicked) {
-				gsap.set("#next-video", { visibility: true });
+				gsap.set("#next-video", { visibility: "visible" });
 				gsap.to("#next-video", {
 					transformOrigin: "center center",
 					scale: 1,
 					width: "100%",
 					height: "100%",
-					duration: 1,
+					duration: 0.8,
 					ease: "power1.inOut",
 					onStart: () => nextVideoRef.current.play()
 				});
 				gsap.from("#current-video", {
 					transformOrigin: "center center",
 					scale: 0,
-					duration: 1.5,
+					duration: 2,
 					ease: "power1.inOut"
 				});
 			}
@@ -51,43 +62,75 @@ const Hero = () => {
 		{ dependencies: [currentIndex], revertOnUpdate: true }
 	);
 
+	useGSAP(() => {
+		gsap.set("#video-frame", {
+			clipPath: "polygon(35% 10%, 60% 4%, 90% 90%, 20% 80%)",
+			borderRadius: "0 0 5% 5%"
+		});
+		gsap.from("#video-frame", {
+			clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+			borderRadius: "0 0 0 0",
+			ease: "power1.inOut",
+			scrollTrigger: {
+				trigger: "#video-frame",
+				start: "center center",
+				end: "bottom top",
+				scrub: true
+			}
+		});
+	});
+
+	const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
+
 	return (
 		<div className="relative h-dvh w-screen overflow-x-hidden">
+			{isLoading && (
+				<div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden">
+					<div className="three-body">
+						<div className="three-body__dot" />
+						<div className="three-body__dot" />
+						<div className="three-body__dot" />
+					</div>
+				</div>
+			)}
+
 			<div id="video-frame" className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75">
 				<div>
-					<div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
+					<div className="mask-clip-path absolute-center absolute z-50 size-52 cursor-pointer overflow-hidden rounded-lg">
 						<div
+							id="mini-video"
 							onClick={handleMiniVdClick}
-							className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100">
+							className="origin-center aspect-square  size-64 scale-50 hover:scale-110 opacity-0 hover:opacity-100 transition-all duration-500  ease-in">
 							<video
-								src={getVideoSrc((currentIndex % totalVideos) + 1)}
-								ref={nextVideoRef}
-								loop
 								id="current-video"
+								ref={nextVideoRef}
+								src={getVideoSrc((currentIndex % totalVideos) + 1)}
+								loop
 								muted
-								autoPlay
-								className="size-64 origin-center scale-150 object-cover object-center"
+								// autoPlay
+								className="size-64 origin-center scale-150    object-cover object-center"
 								onLoadedData={handleVideoLoaded}
 							/>
 						</div>
 					</div>
+
 					<video
-						src={getVideoSrc(currentIndex)}
 						ref={nextVideoRef}
+						src={getVideoSrc(currentIndex)}
 						loop
 						muted
-						autoPlay
+						// autoPlay
 						id="next-video"
-						className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
+						className="absolute-center  invisible absolute z-20 size-64 object-cover object-center"
 						onLoadedData={handleVideoLoaded}
 					/>
 
 					<video
-						src={getVideoSrc(currentIndex === totalVideos + 1 ? 1 : currentIndex)}
+						src={getVideoSrc(1)}
 						autoPlay
 						loop
 						muted
-						className="absolute size-full object-cover object-center"
+						className="absolute left-0 top-0 size-full object-cover object-center"
 						onLoadedData={handleVideoLoaded}
 					/>
 				</div>
