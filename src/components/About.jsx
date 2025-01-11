83056119e2e05400ebdb4dd1de578fb5b4e6bcb2
Lostovayne <moises.barrera.dev@gmail.com>
2025-01-10 23:34:55 -0300

    Update About component's image class for improved styling
    - Modified the class of the image in the About component to include 'overflow-hidden' for better visual presentation.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -44,7 +44,7 @@ const About = () => {
 					<img
 						src="img/about.webp"
 						alt="Background"
-						className="absolute left-0 top-0 size-full object-cover"
+						className="absolute left-0 top-0 size-full object-cover overflow-hidden"
 					/>
 				</div>
 			</div>
