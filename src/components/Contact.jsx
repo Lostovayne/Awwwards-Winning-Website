faa75a12745a38eb62fbf03d75525f4230295cc4
Lostovayne <moises.barrera.dev@gmail.com>
2025-01-13 21:48:29 -0300

    Refactor Contact, Features, and Footer components for improved styling and functionality
    - Adjusted font size in Contact component for better visual consistency.
    - Corrected spelling in Features component title for accuracy.
    - Implemented a new Footer component with social media links and improved layout for better user engagement.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -27,7 +27,7 @@ const Contact = () => {
 
 				<div className="flex flex-col items-center text-center">
 					<p className="font-general text-[10px] uppercase">Join Zentry</p>
-					<p className="special-font mt-10 w-full font-zentry text-[2.7rem] leading-[0.9] md:text-[6rem]">
+					<p className="special-font mt-10 w-full font-zentry text-[2.6rem] leading-[0.9] md:text-[6rem]">
 						Let&apos;s b<b>u</b>ild the <br /> new era of <br /> g<b>a</b>ming t<b>o</b>gether
 					</p>
 					<Button title="contact us" containerClass="mt-10 cursor-pointer" />
--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -85,7 +85,7 @@ const Features = () => {
 						src="videos/feature-1.mp4"
 						title={
 							<>
-								radi<b>n</b>t
+								radia<b>n</b>t
 							</>
 						}
 						description="A cross-platform metagame app, turning your activities acrossWeb2 and Web3 games into a rewarding adventure."
